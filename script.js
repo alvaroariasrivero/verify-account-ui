72adab095265fc98f69f25f43aaadd5fc25a84cf
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-07-09 15:17:17 +0200

    fixed js console errors

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ codes.forEach((code, index) => {
     code.addEventListener('keydown', (e) => {
         if(e.key >= 0 && e.key <= 9){
             codes[index].value = '';
-            setTimeout(() => codes[index + 1].focus(), 10);
-        }else if(e.key === 'Backspace') {
+            if(index < 5){
+                setTimeout(() => codes[index + 1].focus(), 10);
+            }
+        }else if(e.key === 'Backspace' && index > 0) {
             setTimeout(() => codes[index - 1].focus(), 10);
         }
     });
